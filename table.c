af843465ca0f8c5a976cc1af81e23c1253811417
MrJeremyKhoo <jeremykhoois@gmail.com>
2024-03-09 14:17:31 +0800

    RowLinedrawing

--- table.c
+++ table.c
@@ -3,12 +3,13 @@
 
 void drawTable(int rows, int columns, int cellWidth, char data[][ROWCHARSIZE]) {
     // Print the top border
-    for (int i = 0; i < columns * (cellWidth + 1) + 1; i++) {
+        printf(" ");
+    for (int i = 0; i < columns * (cellWidth + 2) + columns - 1; i++) {
         printf("-");
     }
     printf("\n");
 
-    // Print the table content
+    // Print the table contentt
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
             printf("| %-*s ", cellWidth, data[i * columns + j]);
@@ -16,7 +17,8 @@ void drawTable(int rows, int columns, int cellWidth, char data[][ROWCHARSIZE]) {
         printf("|\n");
 
         // Print the row separator
-        for (int k = 0; k < columns * (cellWidth + 1) + 1; k++) {
+        printf(" ");
+        for (int k = 0; k < columns * (cellWidth + 2) + columns - 1; k++) {
             printf("-");
         }
         printf("\n");
